287c22ebe019396e7548aa376bbac0fb4bc73cbf
Sumukh Bhat <sumukhbhat.dev@gmial.com>
2026-05-23 14:36:54 +0530

    initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-5xl font-bold text-green-700">
+        Tailwind Working
+      </h1>
+    </div>
+  )
+}
+
+export default App
